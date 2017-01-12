deeb393fd0877de841fea46000c4d4f210e04a08
Jed <jed-frey@users.noreply.github.com>
2017-01-12 11:55:05 -0500

    Fixed tests

--- tests/test_01.py
+++ tests/test_01.py
@@ -14,9 +14,9 @@ def test_password0():
     hash_type = "sha256"
     known_password="5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5"
     # echo -n 12345 | sha256sum
-    assert known_password == generate(salt=salt,
-                                  separator=separator,
-                                  hash_type=hash_type)
+    assert known_password == generate(salt,
+                                      separator,
+                                      hash_type)
                                   
 def test_password1():
     # "Password" to remember to salt the password generation.
@@ -29,7 +29,7 @@ def test_password1():
     hash_type = "sha256"
     known_password = "d9c9f2f4469fac0870ef4d798e962f18590a19276962ab52e854b0fc756990a2"
     #  echo -n "12345&skroob@spaceball.gov" | sha256sum
-    assert known_password == generate(salt=salt,
-                                      separator=separator,
-                                      hash_type=hash_type,
-                                      email=email)
+    assert known_password == generate(salt,
+                                      separator,
+                                      hash_type,
+                                      email)
